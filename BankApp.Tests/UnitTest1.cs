2cb939dfbd9ee5d726ef97ceec688c373f2b28b4
Ellen Demérus <ellendemerus@hotmail.com>
2019-11-13 14:20:43 +0100

    Added Test-project

---
+++ BankApp.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankApp.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
